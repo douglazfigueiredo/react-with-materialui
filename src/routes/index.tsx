ebed61ae6bce7d8575667ea56c292cdbe71cdfc3
Douglas Figueiredo <douglazfigueiredo@gmail.com>
2022-03-02 23:51:56 -0300

    feat: adicionado responsividade no menu lateral

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@mui/material"
+import { Button, Icon } from "@mui/material"
 import { Routes, Route, Navigate } from "react-router"
-import { useAppThemeContext } from "../shared/contexts"
+import { useDrawerContext } from "../shared/contexts"
 
 export const AppRoutes = () => {
-  const { toggleTheme } = useAppThemeContext()
+  const { toggleDrawerOpen } = useDrawerContext()
 
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}>Toggle Theme</Button>} />
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}><Icon>menu</Icon></Button>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
